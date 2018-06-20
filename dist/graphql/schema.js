81e315bb1bdfab0aed3cf0daf01d62b0a0792444
Vinícius Pereira <jedipereira@gmail.com>
2018-06-20 10:45:28 -0300

    add resolvers to schema using lodash

--- dist/graphql/schema.js
+++ dist/graphql/schema.js
@@ -1,11 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const graphql_tools_1 = require("graphql-tools");
+const lodash_1 = require("lodash");
 const query_1 = require("./query");
 const mutation_1 = require("./mutation");
 const comment_schema_1 = require("./resources/comment/comment.schema");
 const post_schema_1 = require("./resources/post/post.schema");
 const user_schema_1 = require("./resources/user/user.schema");
-const graphql_tools_1 = require("graphql-tools");
+const comment_resolvers_1 = require("./resources/comment/comment.resolvers");
+const post_resolvers_1 = require("./resources/post/post.resolvers");
+const user_resolvers_1 = require("./resources/user/user.resolvers");
+const resolvers = lodash_1.merge(comment_resolvers_1.commentResolvers, post_resolvers_1.postResolvers, user_resolvers_1.userResolvers);
 const SchemaDefinition = `
     type Schema {
         query: Query
@@ -20,5 +25,6 @@ exports.default = graphql_tools_1.makeExecutableSchema({
         comment_schema_1.commentTypes,
         post_schema_1.postTypes,
         user_schema_1.userTypes
-    ]
+    ],
+    resolvers
 });
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,10 +1,21 @@
+import { makeExecutableSchema } from 'graphql-tools'
+import { merge } from 'lodash';
+
 import { Query } from './query';
 import { Mutation } from './mutation';
 import { commentTypes } from './resources/comment/comment.schema';
 import { postTypes } from './resources/post/post.schema';
 import { userTypes } from './resources/user/user.schema';
 
-import { makeExecutableSchema } from 'graphql-tools'
+import { commentResolvers } from './resources/comment/comment.resolvers';
+import { postResolvers } from './resources/post/post.resolvers';
+import { userResolvers } from './resources/user/user.resolvers';
+
+const resolvers = merge(
+    commentResolvers, 
+    postResolvers, 
+    userResolvers
+);
 
 const SchemaDefinition = `
     type Schema {
@@ -21,5 +32,6 @@ export default makeExecutableSchema({
         commentTypes,
         postTypes,
         userTypes
-    ]
+    ],
+    resolvers
 });
