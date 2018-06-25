d34fa02bf6fe4b6abd3ef5c89d46c28afbd86485
Vinícius Pereira <jedipereira@gmail.com>
2018-06-25 08:37:16 -0300

    [fix] fix variable in commentsByPost

--- src/graphql/resources/comment/comment.schema.ts
+++ src/graphql/resources/comment/comment.schema.ts
@@ -16,7 +16,7 @@ const commentTypes = `
 `;
 
 const commentQueries = `
-    commentsByPost(post: ID!, first: Int, offset: Int): [ Comment! ]!
+    commentsByPost(postId: ID!, first: Int, offset: Int): [ Comment! ]!
 `;
 
 const commentMutations = `
