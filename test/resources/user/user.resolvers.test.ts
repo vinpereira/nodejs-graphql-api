a0dc801d193e6148f1c2c9bf5b2e483f7944b8c0
Vinícius Pereira <jedipereira@gmail.com>
2018-07-03 11:22:45 -0300

    [test] create tests for User queries

--- test/resources/user/user.resolvers.test.ts
+++ test/resources/user/user.resolvers.test.ts
@@ -1,17 +1,33 @@
 import { app, chai, db, expect, randomError } from './../../test-utils';
+import { UserInstance } from '../../../src/models/UserModel';
 
 describe('User', () => {
+    let userId: number;
+
     beforeEach(() => {
         return db.Comment.destroy({ where: {} })
             .then((rows: number) => db.Post.destroy({ where: {} }))
             .then((rows: number) => db.User.destroy({ where: {} }))
-            .then((rows: number) => db.User.create(
+            .then((rows: number) => db.User.bulkCreate([
                 {
                     name: "Peter Quill",
                     email: "peter@guardians.com",
                     password: "123"
+                },
+                {
+                    name: "Gamora",
+                    email: "gamora@guardians.com",
+                    password: "123"
+                },
+                {
+                    name: "Groot",
+                    email: "groot@guardians.com",
+                    password: "123"
                 }
-            ));
+            ]))
+            .then((users: UserInstance[]) => {
+                userId = users[0].get('id');
+            });
     });
 
     describe('Queries', () => {
@@ -36,9 +52,134 @@ describe('User', () => {
                         .then(res => {
                             const usersList = res.body.data.users;
                             expect(res.body.data).to.be.an('object');
-                            expect(usersList).to.be.an('array').of.length(1);
+                            expect(usersList).to.be.an('array');
                             expect(usersList[0]).to.not.have.keys(['id', 'photo', 'createdAt', 'updatedAt', 'posts']);
                             expect(usersList[0]).to.have.keys(['name', 'email']);
+                        })
+                        .catch(randomError);
+                });
+
+                it('should paginate a list of Users', () => {
+                    let body = {
+                        query: `
+                            query getUsersLists($first: Int, $offset: Int) {
+                                users(first: $first, offset: $offset) {
+                                    name
+                                    email
+                                    createdAt
+                                }
+                            }
+                        `,
+                        variables: {
+                            first: 2,
+                            offset: 1
+                        }
+                    };
+
+                    return chai.request(app)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .send(JSON.stringify(body))
+                        .then(res => {
+                            const usersList = res.body.data.users;
+                            expect(res.body.data).to.be.an('object');
+                            expect(usersList).to.be.an('array').of.length(2);
+                            expect(usersList[0]).to.not.have.keys(['id', 'photo', 'updatedAt', 'posts']);
+                            expect(usersList[0]).to.have.keys(['name', 'email', 'createdAt']);
+                        })
+                        .catch(randomError);
+                });
+            });
+
+            describe('user', () => {
+                it('should return a single User', () => {
+                    let body = {
+                        query: `
+                            query getSingleUser($id: ID!) {
+                                user(id: $id) {
+                                    id
+                                    name
+                                    email
+                                    posts {
+                                        title
+                                    }
+                                }
+                            }
+                        `,
+                        variables: {
+                            id: userId
+                        }
+                    };
+
+                    return chai.request(app)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .send(JSON.stringify(body))
+                        .then(res => {
+                            const singleUser = res.body.data.user;
+                            expect(res.body.data).to.be.an('object');
+                            expect(singleUser).to.be.an('object');
+                            expect(singleUser).to.have.keys(['id', 'name', 'email', 'posts']);
+                            expect(singleUser.name).to.equal('Peter Quill');
+                            expect(singleUser.email).to.equal('peter@guardians.com');
+                        })
+                        .catch(randomError);
+                });
+
+                it('should return only \'name\' attribute', () => {
+                    let body = {
+                        query: `
+                            query getSingleUser($id: ID!) {
+                                user(id: $id) {
+                                    name
+                                }
+                            }
+                        `,
+                        variables: {
+                            id: userId
+                        }
+                    };
+
+                    return chai.request(app)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .send(JSON.stringify(body))
+                        .then(res => {
+                            const singleUser = res.body.data.user;
+                            expect(res.body.data).to.be.an('object');
+                            expect(singleUser).to.be.an('object');
+                            expect(singleUser).to.have.key('name');
+                            expect(singleUser.name).to.equal('Peter Quill');
+                            expect(singleUser.email).to.be.undefined;
+                            expect(singleUser.createdAt).to.be.undefined;
+                        })
+                        .catch(randomError);
+                });
+
+                it('should return an error if User not exists', () => {
+                    let body = {
+                        query: `
+                            query getSingleUser($id: ID!) {
+                                user(id: $id) {
+                                    name
+                                    email
+                                }
+                            }
+                        `,
+                        variables: {
+                            id: -1
+                        }
+                    };
+
+                    return chai.request(app)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .send(JSON.stringify(body))
+                        .then(res => {
+                            expect(res.body.data.user).to.be.null;
+                            expect(res.body).to.have.keys(['data', 'errors']);
+                            expect(res.body.errors).to.be.an('array');
+                            expect(res.body.errors[0].message).to.be.equal('Error: User with id -1 not found!');
                         })
                         .catch(randomError);
                 });
